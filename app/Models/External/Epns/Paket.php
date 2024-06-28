731c4c8d299d5502d814d2885fdcacfffe272151
Doni Ahmad <donyahmd24@gmail.com>
2024-06-28 14:01:01 +0800

    update connection model

---
+++ app/Models/External/Epns/Paket.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\External\Epns;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Paket extends Model
+{
+    protected $connection = 'pgsql';
+
+    protected $table = 'paket';
+}
