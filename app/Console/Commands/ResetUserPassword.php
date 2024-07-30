eb3985f4acb333d946ed1e9ccdca2c09222b4670
Doni Ahmad <doniahmad1@icloud.com>
2024-07-30 10:30:06 +0800

    reset all user password command (php artisan user:reset-password)

---
+++ app/Console/Commands/ResetUserPassword.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class ResetUserPassword extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:reset-password';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reset all User Password based Current APP_KEY';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Memulai reset password user');
+
+        $userData = User::all();
+
+        $userDataCount = $userData->count();
+        $progressBar   = $this->output->createProgressBar($userDataCount);
+        $progressBar->start();
+
+        foreach ($userData as $user) {
+            $user->update([
+                'password' => Hash::make($user->username),
+            ]);
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+        $this->line('');
+        $this->info('Reset password berhasil');
+    }
+}
