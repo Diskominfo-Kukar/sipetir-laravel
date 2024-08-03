561c9f847598f6343e2ce579a96c61385f87cfaf
IndraW01 <indraakmal08@gmail.com>
2024-08-04 00:30:52 +0800

    Task: Fix Update Panitian

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -15,13 +15,8 @@
 
 <div class="form-group mb-2">
     <label for="{{ $id }}" class="form-label">{{ $label }}</label>
-    <input
-    {{ $disabled ? 'disabled' : '' }}
-    type={{ $type }}
-    name={{ $name }}
-    id="{{ $id }}"
-    {!! $attributes->merge(['class' => $classInput]) !!}
-    />
+    <input {{ $disabled ? 'disabled' : '' }} type={{ $type }} name={{ $name }} id="{{ $id }}"
+        {!! $attributes->merge(['class' => $classInput]) !!} />
     @if ($errors->has($attributes->get('name')))
         <div class="invalid-feedback">{{ ucwords($errors->first($attributes->get('name'))) }}</div>
     @endif
--- resources/views/components/ui/modal.blade.php
+++ resources/views/components/ui/modal.blade.php
@@ -1,48 +1,51 @@
 @props([
-    'id'       => 'my-modal',
-    'title'    => 'Modal',
-    'form'     => true, //bool
-    'action'   => null, //url
+    'id' => 'my-modal',
+    'title' => 'Modal',
+    'form' => true, //bool
+    'action' => null, //url
     'formData' => false, //enctype
-    'button'   => null, // custom button
-    'fallback' => false //reopen modal
+    'button' => null, // custom button
+    'fallback' => false, //reopen modal
+    'formId' => 'formCreate',
 ])
 
 
 
 <!-- Modal -->
-<div class="modal fade" id="{{ $id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div  {!! $attributes->merge(['class' => 'modal-dialog']) !!}>
-    <div class="modal-content">
-      @if($form)
-        <form method="POST" id="form" action="{{ $action }}" @if($formData) enctype="multipart/form-data" @endif>
-          @csrf
-          @if($fallback)
-            <input type="hidden" name="modal_id" value="{{ $id }}">
-            <input type="hidden" name="action_url" id="action_url" value="">
-            <input type="hidden" name="load_url" id="load_url" value="">
-          @endif
-      @endif
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">{!! $title !!}</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+<div class="modal fade" id="{{ $id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div {!! $attributes->merge(['class' => 'modal-dialog']) !!}>
+        <div class="modal-content">
+            @if ($form)
+                <form method="POST" id="{{ $formId }}" action="{{ $action }}"
+                    @if ($formData) enctype="multipart/form-data" @endif>
+                    @csrf
+                    @if ($fallback)
+                        <input type="hidden" name="modal_id" value="{{ $id }}">
+                        <input type="hidden" name="action_url" id="action_url" value="">
+                        <input type="hidden" name="load_url" id="load_url" value="">
+                    @endif
+            @endif
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">{!! $title !!}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
 
-        </button>
-      </div>
-      <div class="modal-body">
-        {{ $slot }}
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-        @if($button)
-          {!! $button !!}
-        @else
-          <button type="submit" class="btn btn-primary submit">Simpan</button>
-        @endif
-      </div>
-      @if($form)
-        </form>
-      @endif
+                </button>
+            </div>
+            <div class="modal-body">
+                {{ $slot }}
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                @if ($button)
+                    {!! $button !!}
+                @else
+                    <button type="submit" class="btn btn-primary submit">Simpan</button>
+                @endif
+            </div>
+            @if ($form)
+                </form>
+            @endif
+        </div>
     </div>
-  </div>
 </div>
